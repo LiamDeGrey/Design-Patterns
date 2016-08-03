b5d02ee497101e258072817dcb351d3fef5d9e8d
Liam De Grey <Liam.DeGrey@gmail.com>
2016-08-04 09:05:59 +1200

    Created initial structure

---
+++ src/experimental/patterns/builder/Main.java
@@ -0,0 +1,17 @@
+package experimental.patterns.builder;
+
+import java.util.Locale;
+
+import experimental.utils.Constants;
+
+/**
+ * Used to initiate the corresponding pattern program
+ * Created by liamdg on 4/08/16.
+ */
+public class Main {
+    private static final String PATTERN_NAME = "Builder";
+
+    public static void main(final String[] args) {
+        System.out.printf(Locale.getDefault(), Constants.STARTUP_MSG, PATTERN_NAME);
+    }
+}
---
+++ src/experimental/utils/Constants.java
@@ -0,0 +1,9 @@
+package experimental.utils;
+
+/**
+ * Used to store values that do not change
+ * Created by liamdg on 4/08/16.
+ */
+public class Constants {
+    public static final String STARTUP_MSG = "Starting %s Pattern";
+}
